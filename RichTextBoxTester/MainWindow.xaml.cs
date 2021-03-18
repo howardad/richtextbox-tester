f89cf5c44f1fac3c3b3f237e09cf80ea88405c7b
Adam Howard <howardad@gmail.com>
2021-03-17 21:33:21 -0400

    Initial commit

---
+++ RichTextBoxTester/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RichTextBoxTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FontSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (rtb != null)
+                rtb.FontSize = Convert.ToDouble(((ComboBoxItem)e.AddedItems[0]).Tag);
+                
+        }
+    }
+}
